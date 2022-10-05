a8eef4b6f12aa40c1a0fd093ed3c69448cd019ce
cboudreau <Chris.Boudreau1999@gmail.com>
2022-10-05 14:00:30 -0300

    created nav list and getting ready for nav to be mobile supported

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from "react"
 import {Link} from 'react-router-dom'
 
 function Nav() {
+  const[click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenue = () => setClick(false);
   return (
     <>
       <nav className='navbar'>
@@ -9,6 +12,35 @@ function Nav() {
           <Link to="\" className="navbar-logo">
             insert logo
           </Link>  
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className= {click ? 'nav-menu active': 'nav-menu'}/>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenue}>
+                  Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenue}>
+                  Photography
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenue}>
+                  Content Creation
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenue}>
+                  Coding 
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenue}>
+                  About Me
+              </Link>
+            </li>
         </div>
       </nav>
     </>
