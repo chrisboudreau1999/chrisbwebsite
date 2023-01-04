d64bfbc1d1a44b44b96b3b4a2f146cdd3c53da93
cboudreau <Chris.Boudreau1999@gmail.com>
2023-01-04 16:23:01 -0400

    edit footer styling and added email button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,16 +6,14 @@ function Footer() {
   return (
     <div className='footer-container'>
         <div className='footer-links'>
-            <div className='footer-link-wrapper'>
-                <div className='footer-link-itmes'>
-                    <a href='https://www.linkedin.com/in/chris-boudreau-6641901b2/'> <i className='fab fa-linkedin' /></a>
-                    <a href='https://www.youtube.com/channel/UCQRuPtG4-PWrCZ9QKTk0qPw'>    <i className='fab fa-youtube' />   </a>
-                    <a href='https://github.com/chrisboudreau1999'>  <i className='fab fa-github' /> </a>
-                    <a href='https://www.instagram.com/chris_b_chiken/'><i className='fab fa-instagram' />  </a>
-                </div>
-            </div>
+
+                    <a href='https://www.linkedin.com/in/chris-boudreau-6641901b2/'><i className='fab fa-linkedin' /></a>
+                    <a href='https://www.youtube.com/channel/UCQRuPtG4-PWrCZ9QKTk0qPw'><i className='fab fa-youtube' /></a>
+                    <a href='https://github.com/chrisboudreau1999'><i className='fab fa-github' /></a>
+                    <a href='https://www.instagram.com/chris_b_chiken/'><i className='fab fa-instagram'  /></a>
+
         </div>
-        
+        <a href = "mailto: chris.boudreau1999@gmail.com">Email Me!</a>
     </div>
   )
 }
