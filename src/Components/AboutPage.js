3b110af96e771f828922c757657202dc65368b7b
cboudreau <Chris.Boudreau1999@gmail.com>
2023-01-24 15:28:45 -0400

    added buttons to about page

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutPage.css';
-
+import Button from 'react-bootstrap/Button';
 function AboutPage(props) {
   return (
     <>
@@ -10,7 +10,21 @@ function AboutPage(props) {
             </div>
             <div className='right'>
               <h1> Chris Boudreau</h1>
-              <h3> Hi I am a 23 year old dal graduate from the Applied Computer Science program.</h3>
+              <h3> Hi I am a 23 year old dal graduate from the Applied Computer Science program! </h3>
+              <Button
+                type="button"
+                variant="primary"
+                onClick={() => {  window.location.href="https://github.com/chrisboudreau1999"; }}
+             >
+               Check out my Github   <i className='fab fa-github '/>     
+             </Button>              
+              <Button
+                type="button"
+                variant="primary"
+                href="/coding" 
+             >
+               Check out my Coding Experiance  <i className='fa fa-code '/>     
+             </Button> 
             </div>
           </div>
 
