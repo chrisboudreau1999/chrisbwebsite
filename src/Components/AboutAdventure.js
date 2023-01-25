9577045567c09b773914236f601899adec56b260
cboudreau <Chris.Boudreau1999@gmail.com>
2023-01-25 12:29:24 -0400

    created intro to adventure page

---
+++ src/Components/AboutAdventure.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './AboutPage.css';
+
+function AboutAdventure(props) {
+  return (
+    <>
+          <div className='about-page'>
+            <div className='left'>
+              <img src='ChirsAdventuretest.jpg' alt='aboutChris'/>
+            </div>
+            <div className='right'>
+              <h3> I am Obssessed with extreme adventure! pushing myself to my limits is something I chase all the time. mainly Biking and backpacking around nova scotia I have seen the beuties of the province and try to capure what there is in store. </h3>
+               </div>
+            </div>
+          
+
+    </>
+  );
+}
+
+export default AboutAdventure;
--- src/Components/pages/Adventure.js
+++ src/Components/pages/Adventure.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../../App.css'
 import Cards from '../AdventureCards.js';
+import AboutAdventure from '../AboutAdventure';
 import Footer from '../Footer';
 function Adventure(){
     return(
         <>
+        <AboutAdventure/>
         <Cards/>
         <Footer />
         </>
