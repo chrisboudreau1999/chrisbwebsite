96bae1059def6e0bb5e054e89b60c2734deb425a
cboudreau <Chris.Boudreau1999@gmail.com>
2023-03-25 11:27:39 -0300

    added couple wording changes andn grammer mistakes

--- src/Components/AdventureCards.js
+++ src/Components/AdventureCards.js
@@ -14,7 +14,7 @@ function AdventureCards() {
                   src='Kenomee.png'
                   text='Kenomee Canyon'
                   label='Backpacking'
-                  popupText='Kenomee Canyon Trail is a backpacking in Economy Nova Scotia with about 30+ kilometers round trip myself and two other friends did this trip in August 2022 Where we made a video showing our experience                  '
+                  popupText='The Kenomee Canyon Trail is a backpacking trail in Economy, Nova Scotia, with about 30+ kilometers round trip. Myself and two other friends did this trip in August 2022, where we made a video showing our experience.'
                   popupLink='https://youtu.be/VDukqaQZBc0'
                   popupButton="Watch the Vlog"
                   popupFA="fab fa-youtube"
@@ -23,7 +23,7 @@ function AdventureCards() {
                   src='Split.jpg'
                   text='Cape Split Sunset'
                   label='Sunset Hike'
-                  popupText="Cape Split is a very heavily trafficked hiking spot in the Annapolis Valley in Nova Scotia, because it is out and back the majority of people don't get to see the beauty of the split during the sunset. Myself and some friends ended up with a perfect sunset back in October 2022 to capture some amazing photos "
+                  popupText="Cape Split is a heavily trafficked hiking spot in the Annapolis Valley of Nova Scotia. Because it is an out-and-back trail, the majority of people don't get to see the beauty of the split during sunset. However, myself and some friends ended up with a perfect sunset back in October 2022, allowing us to capture some amazing photos."
                   popupLink='https://www.alltrails.com/explore/recording/cape-split-trail-08fc80d--2?u=m&sh=sqitha'
                   popupButton='My Cape Split hike'
                   popupFA='fas fa-hiking'
@@ -44,7 +44,7 @@ function AdventureCards() {
                   src='Duncan.jpg'
                   text='Duncans Cove Sunrise'
                   label='Sunrise Hike' 
-                  popupText="Every spring I love to celebrate the weather by visiting Duncans Cove for sunrise. It's a short hike but it shows off the beauty of Nova Scotia's coasts so perfectly I can't resist not doing this hike every year"                  
+                  popupText="Every spring, I love to celebrate the weather by visiting Duncan's Cove for sunrise. It's a short hike, but it shows off the beauty of Nova Scotia's coasts so perfectly that I can't resist doing this hike every year."                  
                   popupLink='https://www.alltrails.com/explore/recording/duncan-s-cove-9bc99c5?u=m&sh=sqitha'
                   popupButton="Duncan's Cove Sunrise"
                   popupFA='fas fa-hiking'
@@ -53,7 +53,8 @@ function AdventureCards() {
                   src='Chignecto.jpg'
                   text='Cape Chignecto'
                   label='Backpacking' 
-                  popupText='Cape Chignecto is considered to be one of the most challenging backpacking routes in Nova Scotia, with 52 km and 2000+ Meters of elevation this trip is quite difficult, Myself and 3 of my friends managed to complete this challenge in less than 72 hours while capturing all of the struggles and highlights'   popupLink='https://www.youtube.com/playlist?list=PLkdlS2FLiv46autS0W_vxaS6CfCTFwVcr'
+                  popupText='Cape Chignecto is considered to be one of the most challenging backpacking routes in Nova Scotia. With 52 km and 2000+ meters of elevation, this trip is quite difficult. However, myself and three of my friends managed to complete this challenge in less than 72 hours while capturing all of the struggles and highlights.'  
+                   popupLink='https://www.youtube.com/playlist?list=PLkdlS2FLiv46autS0W_vxaS6CfCTFwVcr'
                   popupButton='Watch the Vlog'
                   popupFA="fab fa-youtube"
              />
--- src/Components/CardsCC.js
+++ src/Components/CardsCC.js
@@ -13,7 +13,7 @@ function CardsCC() {
               src='KoH.png'
               text='Kings of Hali 5, Melee Stream Runner'
               label='LiveStream Running'
-              popupText='during big events you need mulitple people to help with broadcasting. during Kinngs of Hali 5 the biggest fighting game tourniment of atlantic canada I was a volenteer stream runner with jobs including,finding people to go on stream, updating the stream data on the players on screen, annd moderation of the chat'
+              popupText='During big events, you need multiple people to help with broadcasting. During Kings of Hali 5, the biggest fighting game tournament of Atlantic Canada, I was a volunteer stream runner with jobs including finding people to go on the stream, updating the stream data on the players on screen, and moderating the chat.'
               popupLink='https://www.twitch.tv/videos/1561318316?t=03h29m54s'
               popupButton='Checkout the Kings of Hali 5 stream'
               popupFA='fab fa-twitch'
@@ -22,7 +22,7 @@ function CardsCC() {
               src='ChessBrah.jpg'
               text='Chessbruh plays me and puts me on his youtube channel'
               label='Feature'
-              popupText='I had the pleasure of playing a casual game of chess with Grandmaster Aman Hambleton and he decided to feature that game on his youtube channel ChessBruh'
+              popupText='I had the pleasure of playing a casual game of chess with Grandmaster Aman Hambleton, and he decided to feature that game on his YouTube channel, ChessBruh.'
               popupLink='https://www.youtube.com/watch?v=wsC6RrjAQus'
               popupButton="watch the video here"
               popupFA="fab fa-youtube"
@@ -34,7 +34,7 @@ function CardsCC() {
               src='skeleton.jpg'
               text='Level22Skeleton Interview people downtown'
               label='Camera Work'
-              popupText='I had the pleasure to be the cameraman for this interview. we got to experience a lot of the the Halifax Downtown culture and captured the moment and got to interview some very interesting people'
+              popupText='I had the pleasure of being the cameraman for this interview. We got to experience a lot of the Halifax Downtown culture, captured the moment, and interviewed some very interesting people.'
               popupLink="https://www.youtube.com/watch?v=WYS1PlSZDbo"
               popupButton="watch the video here"
               popupFA="fab fa-youtube"
@@ -43,7 +43,7 @@ function CardsCC() {
               src='tmrw.jpg'
               text='TomorrowLan 2022 Casting'
               label='Livestream Casting'
-              popupText='Casting is very important for tourniments of all varities and commentary is a very hard task. throughout the years got to cast multiple super smash brothers melee tourniments gainning skills in beingn entertainning and knowledgeble for viewers to enjoy live streams'
+              popupText='Casting is very important for tournaments of all varieties, and commentary is a very hard task. Throughout the years, I got to cast multiple Super Smash Brothers Melee tournaments, gaining skills in being entertaining and knowledgeable for viewers to enjoy live streams.'
               popupLink='https://www.twitch.tv/videos/1756713172?t=02h42m37s'
               popupButton="Check out my latest casting!"
               popupFA='fab fa-twitch'
