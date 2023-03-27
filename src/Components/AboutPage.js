02c745c83506cc3d8f308b18f080495b6d29ac06
cboudreau <Chris.Boudreau1999@gmail.com>
2023-03-27 13:17:27 -0300

    some more bug fixes

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AboutPage.css';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 function AboutPage(props) {
   return (
     <>
@@ -10,17 +11,13 @@ function AboutPage(props) {
             </div>
             <div className='right'>
               <h1> Chris Boudreau</h1>
-              <h3> Hi I am a 23 year old dal graduate from the Applied Computer Science program! this webiste is to so show off all my passions including things like Chess, Biking, Photography, and especially Programming! I am very passionate about Web Development and coding in general, check out my coding experiance with these links! </h3>
+              <h3> Hi I am a 23 year old dal graduate from the Applied Computer Science program! this website is to so show off all my passions including things like Chess, Biking, Photography, and especially Programming! I am very passionate about Web Development and coding in general, check out my coding experiance with these links! </h3>
               <div className='.btn'>
               <Button href={"https://github.com/chrisboudreau1999"} target="_blank" rel="noopener noreferrer"
              >
                Check out my Github   <i className='fab fa-github '/>     
              </Button>    {'  '}           
-              <Button
-                type="button"
-                variant="primary"
-                href="/Coding" 
-             >
+             <Button as={Link} to="/coding" variant="primary">
                Check out my Coding Experiance  <i className='fa fa-code '/>     
              </Button> 
                </div>
